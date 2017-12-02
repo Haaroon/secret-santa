0417a950ab6ce29810bef42fce5106b8291968b9
Haaroon <haaroon.yousaf@gmail.com>
2017-12-02 14:32:07 +0000

    Add HTML to the page, cause everyone likes an xmas cat html, remove dont-pair cause humans must set aside their differences in the holiday spirit, add membersof.rb is a script i found online that gets you the firstname,lastname,email in a csv from a slack channel, edit secret_santa to accept html, remove usage of dont-pair, fixed issue when it wouldnt add the message to the body

---
+++ membersof.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'open-uri'
+require 'json'
+require 'csv'
+
+options = {}
+ARGV << '-h' if ARGV.empty?
+OptionParser.new do |opts|
+  opts.banner = "This script retrieves all members from a slack channel\nUsage: #{$PROGRAM_NAME} [options]"
+  opts.on('-t', '--token Slack tocken', 'How to get: https://api.slack.com/custom-integrations/legacy-tokens') { |v| options[:token] = v }
+  opts.on('-g', '--group Group ID', 'How to get: https://api.slack.com/methods/groups.list') { |v| options[:group_id] = v }
+  opts.on('-c', '--channel Channel ID', 'How to get: https://api.slack.com/methods/channels.list') { |v| options[:channel_id] = v }
+end.parse!
+
+unless options[:token].nil?
+  mambers = nil
+  mambers = JSON.parse(open("https://slack.com/api/channels.info?token=" + options[:token] + "&channel=" + options[:channel_id]).read)['channel']['members'] unless options[:channel_id].nil?
+  mambers = JSON.parse(open("https://slack.com/api/groups.info?token=" + options[:token] + "&channel=" + options[:group_id]).read)['group']['members'] unless options[:group_id].nil?
+  if mambers
+    csv_string = CSV.generate do |csv|
+      csv_header = ['first_name','email']
+      csv << csv_header
+      mambers.each do |member_id|
+        profile = JSON.parse(open("https://slack.com/api/users.info?token=" + options[:token] + "&user=" + member_id).read)['user']
+        csv << profile['profile'].select { |key,_| csv_header.include? (key) }.values unless profile['deleted']
+      end
+    end
+    puts csv_string
+  end
+end
+
